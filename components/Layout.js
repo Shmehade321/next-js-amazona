ba26296aa3f695d5bee9ca4871feaa40ac102893
Mehade Hasan <hasanmehade300@gmail.com>
2021-09-17 22:41:20 -0400

    Initial project commit

---
+++ components/Layout.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Head from "next/head";
+import NextLink from "next/link";
+import { AppBar, Container, Link, Toolbar, Typography } from "@mui/material";
+import useStyles from "../utils/styles";
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Head>
+        <title>Next Amazona</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Next Amazona.</Typography>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
